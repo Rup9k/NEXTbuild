c53779518a37b8450152ac9b4715ed6eec125731
Rup9k <dimapastuhovaz@gmail.com>
2025-12-19 07:47:52 +0400

    добавил админку

--- nextbuild-project/backend/config/urls.py
+++ nextbuild-project/backend/config/urls.py
@@ -1,6 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.models import User
+
+admin.site.register(User)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
